05193b664eb827bc57043075c9675a42168206a5
Karen Guatumillo <100394222+Karen-Mich@users.noreply.github.com>
2024-05-06 00:18:51 -0500

    Create actualizar.php

---
+++ Models/actualizar.php
@@ -0,0 +1,23 @@
+<?php
+class crudA
+{
+    public static function ActualizarEstudiantes()
+    {
+        include_once ('conexion.php');
+        header('Content-Type: application/json');
+        $objeto = new conexion();
+        $conectar = $objeto->conectar();
+        //$data = json_decode(file_get_contents("php://input"), true);
+        $cedula = $_GET["cedula"];
+        $nombre = $_GET["nombre"];
+        $apellido = $_GET["apellido"];
+        $direccion = $_GET["direccion"];
+        $telefono = $_GET["telefono"];
+        $actualizarSql = "UPDATE estudiante SET nombre='$nombre', apellido='$apellido', direccion='$direccion', telefono='$telefono' WHERE cedula = '$cedula' ";
+        $resultado = $conectar->prepare($actualizarSql);
+        $resultado->execute();
+        echo json_encode($resultado);
+        $conectar->commit();
+    }
+}
+?>
